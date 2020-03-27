53d4f6ac0151c08f49a2ac2757ad6ddf48a00972
纪猛 <927994432@qq.com>
2020-03-27 15:51:56 +0800

    给出服务者注册示例

--- src/appProvider.js
+++ src/appProvider.js
@@ -1,16 +1,19 @@
-import { Provider } from 'rc-xblock'
+import {Provider} from 'rc-xblock'
 import Fetch from 'rc-xblock/fetch'
 
 export default class appProvider extends Provider {
 
-  boot () {
-    Fetch.headers = {
-      company: 'eryun'
-    }
-
+  /**
+   * 应用启动前,在这里进行初始化操作
+   */
+  boot() {
+    // console.log(this.app)
   }
 
-  register () {
+  /**
+   * 应用启动后，可以在这进行组件注册等
+   */
+  register() {
 
     // console.log(this.app)
   }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 document.getElementById('nio-loading').style.display = 'none'
 import XBlock from 'rc-xblock'
+import appProvider from 'appProvider'
 import 'antd/dist/antd.less'
 import 'rc-xblock/styles/index.less'
+
+//注册服务者
+XBlock.provider(appProvider)
+
+//启动应用
 XBlock.run('#root')
+
